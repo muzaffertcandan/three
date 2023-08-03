fe69dd68bc26c2fbb3501f125ae1c3a06170c6c4
muzaffertcandan <muzaffertcandan@gmail.com>
2023-08-03 15:45:11 +0300

    10 devam ediyor

--- 10-debug-ui/src/script.js
+++ 10-debug-ui/src/script.js
@@ -1,6 +1,9 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import gsap from 'gsap'
+import * as dat from 'dat.gui';
+
+console.log(dat);
 
 /**
  * Base
@@ -11,48 +14,17 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-// Object
-
-// const positionsArray = new Float32Array(9)
-// //first vertice x,y,z
-// positionsArray[0] = 0
-// positionsArray[1] = 0
-// positionsArray[2] = 0
-// //second vertice x,y,z
-// positionsArray[3] = 0
-// positionsArray[4] = 1
-// positionsArray[5] = 0
-
-// positionsArray[6] = 1
-// positionsArray[7] = 0
-// positionsArray[8] = 0
-const geometry = new THREE.BufferGeometry()
-
-const count = 500
-//bu kısmı karıştırma 
-const positionsArray = new Float32Array(count * 3 * 3)
-for(let i = 0; i < count *3 *3; i++){
-    positionsArray[i] = (Math.random() -0.5) *4
-}
-
-const positionAttribute = new THREE.BufferAttribute('positionsArray', 3)
-geometry.setAttribute('positions', positionAttribute)
-// const positionsArray = new Float32Array([
-//     0,0,0,
-//     0,1,0,
-//     1,0,0
-// ])
-// const positionAttribute = new THREE.BufferAttribute(positionsArray,3)
-
-// geometry.setAttribute('position', positionAttribute)
-
-// const geometry = new THREE.BoxGeometry(1, 1, 1, 4,4,4)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000,
-wireframe: true })
+/**
+ * Object
+ */
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
-// Sizes
+/**
+ * Sizes
+ */
 const sizes = {
     width: window.innerWidth,
     height: window.innerHeight
@@ -73,7 +45,10 @@ window.addEventListener('resize', () =>
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-// Camera
+/**
+ * Camera
+ */
+// Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.z = 3
 scene.add(camera)
@@ -82,14 +57,18 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-// Renderer
+/**
+ * Renderer
+ */
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-// Animate
+/**
+ * Animate
+ */
 const clock = new THREE.Clock()
 
 const tick = () =>
